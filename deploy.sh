573cb48cf578394788bcaac7fb0486e82e614666
Lalitha011004 <127740404+Lalitha011004@users.noreply.github.com>
2025-02-05 10:11:34 +0530

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-    echo Hello
-    sh 'chmod +x build.sh'
-    sh './build.sh'
-    docker login 
-    docker tag test lalithambigai011004/task2
-    docker push lalithambigai011004/task2
-    
