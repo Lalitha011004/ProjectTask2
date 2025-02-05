0cde9e7f48ec8ed0846481439a6276ea3e013b76
Lalitha011004 <127740404+Lalitha011004@users.noreply.github.com>
2025-02-05 10:21:26 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-     docker login 
+    docker login -u lalithambigai011004 -p Lali_0121
     docker tag test lalithambigai011004/task2
     docker push lalithambigai011004/task2
     
